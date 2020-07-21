68345f459a376a4384795c68d037d07b61b71fcf
Robbert Sinclair <56741833+RobbertSinclair@users.noreply.github.com>
2020-07-21 11:31:40 +0100

    Update main.py
    Added a function to draw the chessboard in the pygame window.

--- main.py
+++ main.py
@@ -10,6 +10,25 @@ screen = pygame.display.set_mode((800,800))
 WHITE = (255, 255, 255)
 BLACK = (0,0,0)
 
+#Define the areas to fill the board
+def board_fill(width = 800, height = 800):
+    positions = []
+    zero_start = True
+    the_height = 0
+    while the_height < height:
+        i = 0
+        if zero_start:
+            the_start = 0
+        else:
+            the_start = 100
+        while i < width:
+            new_tuple = (the_start + i, the_height, 100, 100)
+            positions.append(new_tuple)
+            i += 200
+        the_height += 100
+        zero_start = not zero_start
+    return positions
+
 #caption
 pygame.display.set_caption("Chess")
 
@@ -19,11 +38,18 @@ y = 0
 gameExit = False
 
 while not gameExit:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            gameExit =  True
-        else:
-            print("Game is playing")
+    event = pygame.event.poll()
+    if event.type == pygame.QUIT:
+        gameExit = True
+    
+    screen.fill(WHITE)
+
+    the_positions = board_fill()
+    for position in the_positions:
+        screen.fill(BLACK, position)
+
+
+    pygame.display.flip()
 
 
 pygame.quit()
