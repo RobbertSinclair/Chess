c900c284ae6a431a43da530b394442db25bd91b9
Robbert Sinclair <56741833+RobbertSinclair@users.noreply.github.com>
2020-07-24 12:10:45 +0100

    Update piece.py
    Added the logic to check whether a player can go to a location depending on whether there is an allying piece or an enemy piece. Also changed the pawn class so that the pawn can only take in a diagonal formation.

--- piece.py
+++ piece.py
@@ -54,6 +54,13 @@ class Pawn(Piece):
 
     def __str__(self):
         return "P"
+    
+    def checkOccupied(self, position, input_board):
+        piece = input_board[position]["piece"]
+        if piece == 0:
+            return False
+        else:
+            return True
 
     def checkTake(self, input_board):
         possible_takes = []
@@ -72,9 +79,8 @@ class Pawn(Piece):
                 possible_takes.append(pos)
         return possible_takes
 
-        
-    
     def getAllowedMoves(self, input_board):
+        moves = []
         if self.side == 0:
             mult = (-1, -2)
         else:
@@ -82,12 +88,16 @@ class Pawn(Piece):
         print(self.checkTake(input_board))
         if self.y + mult[0] >= 0 and self.y + mult[1] <= 7:
             if self.x == self.start_x and self.y == self.start_y:
-                
-                return [(self.x, self.y+mult[0]), (self.x, self.y+mult[1])] + self.checkTake(input_board)
+                pot_moves = [(self.x, self.y+mult[0]), (self.x, self.y+mult[1])]
             else:
-                return [(self.x, self.y+mult[0])] + self.checkTake(input_board)
+                pot_moves = [(self.x, self.y+mult[0])]
+            print(f"The potential main moves are {pot_moves}")
         else:
             return []
+        for move in pot_moves:
+            if not self.checkOccupied(move, input_board):
+                moves.append(move)
+        return moves + self.checkTake(input_board)
 
 
 class Rook(Piece):
@@ -133,7 +143,8 @@ class Bishop(Piece):
         for x in range(8):
             for y in range(8):
                 if abs(self.x - x) == abs(self.y - y) and (self.x != x and self.y != y):
-                    moves.append((x, y))
+                    if not self.checkAllyOccupied((x,y), input_board):
+                        moves.append((x, y))
         return moves
 
 class Queen(Piece):
@@ -153,7 +164,8 @@ class Queen(Piece):
         for x in range(8):
             for y in range(8):
                 if abs(self.x - x) == abs(self.y - y) and (self.x != x and self.y != y):
-                    moves.append((x, y))
+                    if not self.checkAllyOccupied((x, y), input_board):
+                        moves.append((x, y))
         return moves
     
     def straight(self, input_board):
@@ -163,7 +175,12 @@ class Queen(Piece):
         endRangeY = 7 - self.y
         x_moves = [(self.x + i, self.y) for i in range(startRangeX, endRangeX + 1)]
         y_moves = [(self.x, self.y + i) for i in range(startRangeY, endRangeY + 1)]
-        return x_moves + y_moves
+        the_moves = x_moves + y_moves
+        for coord in the_moves:
+            if self.checkAllyOccupied(coord, input_board):
+                the_index = the_moves.index(coord)
+                the_moves.pop(the_index)
+        return the_moves
 
     def getAllowedMoves(self, input_board):
         return self.diagonal(input_board) + self.straight(input_board)
@@ -194,7 +211,7 @@ class King(Piece):
         
         for diff in differences:
             new_move = (self.x + diff[0], self.y + diff[1])
-            if (new_move[0] >= 0 and new_move[1] <= 7) and (new_move[1] >= 0 and new_move[1] <= 7):
+            if (new_move[0] >= 0 and new_move[1] <= 7) and (new_move[1] >= 0 and new_move[1] <= 7) and not self.checkAllyOccupied(new_move, input_board):
                 moves.append(new_move)
         return moves
         
@@ -222,6 +239,6 @@ class Knight(Piece):
                       (-1,-2)]
         for diff in difference:
             new_move = (self.x + diff[0], self.y + diff[1])
-            if (new_move[0] >= 0 and new_move[0] <= 7) and (new_move[1] >= 0 and new_move[1] <= 7):
+            if (new_move[0] >= 0 and new_move[0] <= 7) and (new_move[1] >= 0 and new_move[1] <= 7) and not self.checkAllyOccupied(new_move, input_board):
                 moves.append(new_move)
         return moves
