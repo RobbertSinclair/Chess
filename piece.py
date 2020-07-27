dd8b807d52b30d5d572aea47d5de90754b931ad4
Robbert Sinclair <56741833+RobbertSinclair@users.noreply.github.com>
2020-07-27 15:57:55 +0100

    Update piece.py
    Added the functionality in the king class so that the king cannot bring itself into check

--- piece.py
+++ piece.py
@@ -51,11 +51,9 @@ class Piece():
         moves = []
         position = self.getPosition()
         move_diff = [coord[0] - position[0], coord[1] - position[1]]
-        print(f"Move diff before: {move_diff}")
         for i in range(len(move_diff)):
             if move_diff[i] != 0:
                 move_diff[i] = move_diff[i] // abs(move_diff[i])
-        print(f"Move diff after: {move_diff}")
         mult = 0
         new_move = (0,0)
         while (new_move[0] >= 0 and new_move[0] <= 7) and (new_move[1] >= 0 and new_move[1] <= 7):
@@ -79,15 +77,12 @@ class Piece():
         y_moves.pop(current)
         the_moves = x_moves + y_moves
         for coord in the_moves:
-            print("Loop")
             if input_board[coord]["piece"] != 0:
                 remove = remove + self.removePlaces(coord)
-                print(remove)
                 if not self.checkAllyOccupied(coord, input_board):
                     index = remove.index(coord)
                     remove.pop(index)    
         the_moves = self.removeElts(the_moves, remove)
-        print(f"The possible moves are {the_moves}")
         return the_moves
 
     def diagonal(self, input_board):
@@ -137,31 +132,29 @@ class Pawn(Piece):
             if (self.x + di[0] >= 0 and self.x + di[0] <= 7) and (self.y + di[1] >= 0 and self.y + di[1] <= 7):
                 position = (self.x + di[0], self.y + di[1])
                 pot_positions.append(position)
-        print(f"The potential diagonal positions are {pot_positions}")
         for pos in pot_positions:
             if input_board[pos]["piece"] != 0 and input_board[pos]["piece"].getSide() != self.side:
                 possible_takes.append(pos)
         return possible_takes
 
-    def getAllowedMoves(self, input_board):
+    def getAllowedMoves(self, input_board, side_pieces):
         moves = []
         if self.side == 0:
             mult = (-1, -2)
         else:
             mult = (1, 2)
-        print(self.checkTake(input_board))
         if self.y + mult[0] >= 0 and self.y + mult[1] <= 7:
             if self.x == self.start_x and self.y == self.start_y:
                 pot_moves = [(self.x, self.y+mult[0]), (self.x, self.y+mult[1])]
             else:
                 pot_moves = [(self.x, self.y+mult[0])]
-            print(f"The potential main moves are {pot_moves}")
         else:
             return []
         for move in pot_moves:
             if not self.checkOccupied(move, input_board):
                 moves.append(move)
         return moves + self.checkTake(input_board)
+        
 
 class Rook(Piece):
 
@@ -175,7 +168,7 @@ class Rook(Piece):
     def __str__(self):
         return "R"
 
-    def getAllowedMoves(self, input_board):
+    def getAllowedMoves(self, input_board, side_pieces):
         return self.straight(input_board)
 
 class Bishop(Piece):
@@ -190,8 +183,9 @@ class Bishop(Piece):
     def __str__(self):
         return "B"
 
-    def getAllowedMoves(self, input_board):
+    def getAllowedMoves(self, input_board, side_pieces):
         return self.diagonal(input_board)
+        
 
 class Queen(Piece):
 
@@ -205,7 +199,7 @@ class Queen(Piece):
     def __str__(self):
         return "Q"
 
-    def getAllowedMoves(self, input_board):
+    def getAllowedMoves(self, input_board, side_pieces):
         return self.diagonal(input_board) + self.straight(input_board)
 
 class King(Piece):
@@ -216,27 +210,49 @@ class King(Piece):
             self.icon = "white_king.png"
         else:
             self.icon = "black_king.png"
-
+    
     def __str__(self):
         return "K"
+    
+    def removeCheckAreas(self, input_board, side_pieces, moves, side_turn):
+        remove = []
+        if self.getSide() == 0:
+            enemy_side = 1
+        else:
+            enemy_side = 0
+        for piece in side_pieces[enemy_side]:
+            if str(piece) == "P":
+                remove = remove + piece.checkTake(input_board)
+            else:
+                if str(piece) != "K":
+                    enemyMoves = piece.getAllowedMoves(input_board, side_pieces)
+                else:
+                    enemyMoves = piece.getAllowedMoves(input_board, side_pieces, side_turn)
+                for move in enemyMoves:
+                    if move in moves:
+                        remove.append(move)    
+        return remove
 
-    def getAllowedMoves(self, input_board):
+    def getAllowedMoves(self, input_board, side_pieces, side_turn):
         moves = []
-        #Consider all of the possible moves
         differences = [(1,0),
-                       (0,1),
-                       (1,1),
-                       (-1,0),
-                       (0,-1),
-                       (1,-1),
-                       (-1,1),
-                       (-1,-1)]
-        
+                    (0,1),
+                    (1,1),
+                    (-1,0),
+                    (0,-1),
+                    (1,-1),
+                    (-1,1),
+                    (-1,-1)]    
         for diff in differences:
             new_move = (self.x + diff[0], self.y + diff[1])
             if (new_move[0] >= 0 and new_move[1] <= 7) and (new_move[1] >= 0 and new_move[1] <= 7) and not self.checkAllyOccupied(new_move, input_board):
                 moves.append(new_move)
+        if len(moves) != 0 and side_turn == self.getSide():
+            remove = self.removeCheckAreas(input_board, side_pieces, moves, side_turn)
+            print(f"Remove is {remove}")
+            moves = self.removeElts(moves, remove)
         return moves
+        
         
 class Knight(Piece):
 
@@ -250,16 +266,16 @@ class Knight(Piece):
     def __str__(self):
         return "N"
 
-    def getAllowedMoves(self, input_board):
+    def getAllowedMoves(self, input_board, side_pieces):
         moves = []
         difference = [(2,1),
-                      (2,-1),
-                      (-2,1),
-                      (-2,-1),
-                      (1,2),
-                      (1,-2),
-                      (-1,2),
-                      (-1,-2)]
+                    (2,-1),
+                    (-2,1),
+                    (-2,-1),
+                    (1,2),
+                    (1,-2),
+                    (-1,2),
+                    (-1,-2)]
         for diff in difference:
             new_move = (self.x + diff[0], self.y + diff[1])
             if (new_move[0] >= 0 and new_move[0] <= 7) and (new_move[1] >= 0 and new_move[1] <= 7) and not self.checkAllyOccupied(new_move, input_board):
